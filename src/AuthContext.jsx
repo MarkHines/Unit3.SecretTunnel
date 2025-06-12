8610e3c2d2c79cf40d49fe05a66005ea8b3578dd
Mark Hines <hines3521@gmail.com>
2025-06-12 18:59:37 -0400

    edit SecretTunnel app

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -5,14 +5,66 @@ const API = "https://fsa-jwt-practice.herokuapp.com";
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [token, setToken] = useState();
+  const [token, setToken] = useState(localStorage.getItem(`token`));
   const [location, setLocation] = useState("GATE");
-
+  const [userName, setUserName] = useState(` `);
+  
   // TODO: signup
 
+  const signUp = async(event) => {
+    event.preventDefault();
+
+    if(userName === ` `){
+      alert(`Please Enter a Name!`)
+      throw Error (`error`)
+    }
+    const response = await fetch(API + `/signup`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: userName,
+        password: `secrettunnel`,
+      })
+    })
+    const tokenObject = await response.json()
+    //console.log(tokenObject);
+    localStorage.setItem(`token`, tokenObject.token);
+    setToken(tokenObject.token);
+    setLocation("TABLET")
+  }
   // TODO: authenticate
 
-  const value = { location };
+  const authenticate = async(event) => {
+    event.preventDefault();
+    //console.log(token);
+    if(!token){
+      throw Error (`No token provided`)
+    }
+    else {
+      const response = await fetch(API + `/authenticate`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        }
+      })
+      const responseObject = await response.json()
+      //console.log(responseObject);
+      if(!responseObject.success) {
+        throw Error (`Failed to Authenticate`);
+      }
+      else {
+        setLocation("TUNNEL")
+      }
+    }
+  }
+
+  const value = { 
+    location, userName, setUserName, 
+    signUp, authenticate
+  };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
--- src/Entrance.jsx
+++ src/Entrance.jsx
@@ -1,6 +1,10 @@
+import {  useAuth } from './AuthContext'
+
 /** Users can enter their name to receive a token from the API. */
 export default function Entrance() {
   // TODO: call signup when form is submitted
+  
+  const {setUserName, signUp} = useAuth()
 
   return (
     <>
@@ -15,10 +19,12 @@ export default function Entrance() {
         fixed on you. The one on the left opens its mouth, and with a deep,
         rumbling voice, it asks, "Who approaches? Speak your name."
       </p>
-      <form>
+      <form onSubmit={ signUp }>
         <label>
           Name
-          <input name="name" />
+          <input 
+            name="name"
+            onChange={(event) => { setUserName(event.target.value)}} />
         </label>
         <button>Respond</button>
       </form>
